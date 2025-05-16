1003564a4ebd8641744c8129c738cb313d4b5001
AbhilekhGautam <abhilekhgautam119@gmail.com>
2025-05-16 19:21:45 +0000

    optimize the instr based on the lvn entries

--- src/lvn/lvn.py
+++ src/lvn/lvn.py
@@ -1,7 +1,12 @@
 import json
 import sys
+import os
 
-COMMUTATIVE_OPERATOR = 'add', 'mul', 'sub'
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+from src.dce.dce import dce
+
+COMMUTATIVE_OPERATOR = 'add', 'mul'
 
 class lvn_table_value:
     def __init__(self, op, value, index1, index2):
@@ -40,11 +45,15 @@ class lvn_table:
         return True
 
 def lvn(function):
+    # hold the current state (environment)
     name2index = {}
     my_table = lvn_table()
+    # index
     count = 0
     for instr in function['instrs']:
+        # only do if not labels
         if 'op' in instr:
+            # case 1: const value
             if 'value' in instr:
                 val = lvn_table_value(instr['op'], instr['value'], None, None)
                 if 'dest' in instr:
@@ -60,18 +69,28 @@ def lvn(function):
                         my_table.add_to_table(count, val, None)
                     
                     name2index[instr['dest']] = count
-
+            # case 2: variable reference
             elif 'args' in instr:
+                # Check if the operand is in the current environment
                 result = [name2index.get(item, item) for item in instr['args']]
+                # Check If the operator takes 2 operands
                 if len(result) == 2 and result[1] not in name2index:
-                    if result[0] < result[1]:
-                        val = lvn_table_value(instr['op'], None, result[0], result[1])
-                    else:
+                    # handle commutative property: smaller index first and then larger
+                    if instr['op'] in COMMUTATIVE_OPERATOR and result[0] > result[1]:
                         val = lvn_table_value(instr['op'], None, result[1], result[0])
+                    else:
+                        val = lvn_table_value(instr['op'], None, result[0], result[1])
+
                     if 'dest' in instr:
+                        # If value is already present, no new table entry
                         if my_table.doesnot_have(val):
                             count = count + 1
                             my_table.add_to_table(count, val, instr['dest'])
+                            canonical_name_first = my_table.table[result[0]][1]
+                            canonical_name_second = my_table.table[result[1]][1]
+                            instr['args'][0] = canonical_name_first
+                            instr['args'][1] = canonical_name_second
+
                         name2index[instr['dest']] = count
 
                 elif len(result) == 2 and result[1] in name2index:
@@ -83,14 +102,17 @@ def lvn(function):
                         if my_table.doesnot_have(val):
                             count = count + 1
                             my_table.add_to_table(count, val, instr['dest'])
+                            # Get the canonical name for index in the lvn_value tuple
+                            canonical_name = my_table.table[result[0]][1]
+                            instr['args'][0] = canonical_name
+
                         
                         name2index[instr['dest']] = count
 
                 elif result[0] in name2index:
                     name2index[instr['dest']] = count
+
     
-    for key, value in name2index.items():
-        print(f"{key}: {value}")
                  
     return my_table
 
@@ -98,8 +120,8 @@ def lvn(function):
 def entry():
     prog = json.load(sys.stdin)
     for function in prog['functions']:
-        table = lvn(function)
-        table.print_table()
+        lvn(function)
+    return json.dump(prog, sys.stdout)
 
 if __name__ == '__main__':
     entry()
